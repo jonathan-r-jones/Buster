112a7d939682e5b143ce2db9169b21ff537b131a
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-07-31 16:43:35 -0400

    Jul-31-2018_4:43_PM

--- Buster/Buster/App.xaml.cs
+++ Buster/Buster/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation (XamlCompilationOptions.Compile)]
@@ -10,15 +14,14 @@ namespace Buster
 		public App ()
 		{
 			InitializeComponent();
-
-
 			MainPage = new Views.MainPage();
 		}
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
-		}
+            // Handle when your app starts
+            AppCenter.Start("android=296f3ec7-9915-4f7a-abbe-13b3c3330ac8;" + "uwp={Your UWP App secret here};" + "ios=d595c25f-8329-4cbd-8869-939bd9e119f6;", typeof(Analytics), typeof(Crashes));
+        }
 
 		protected override void OnSleep ()
 		{
